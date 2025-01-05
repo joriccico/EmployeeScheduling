8340017d19bf79bd8ec70bb0fa799059c3764078
joriccico <josep.ricci0@gmail.com>
2025-01-05 23:55:50 +0100

    requirements file + mappings for data conversion simplification

---
+++ src/mappings.py
@@ -0,0 +1,8 @@
+# mappings.py
+class Mappings:
+    SHIFT = {"libre": 0, "mañana": 1, "tarde": 2, "noche": 3}
+    DAYS = {"lunes": 0, "martes": 1, "miércoles": 2, "jueves": 3, "viernes": 4, "sábado": 5, "domingo": 6}
+    EMPLOYEES = {
+        "Pepito": 0, "Juanita": 1, "Carlos": 2, "Ana": 3,
+        "Maria": 4, "Miguel": 5, "Juan": 6, "Sara": 7
+    }
